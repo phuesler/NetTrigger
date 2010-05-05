9d03e3987ab6054c7a4373c32c1654f373016f60
Patrick Huesler <patrick.huesler@gmail.com>
2010-05-05 19:17:56 +0200

    Removing tracking of task completion. Don't need it right now

--- NetTriggerDelegate.rb
+++ NetTriggerDelegate.rb
@@ -3,10 +3,6 @@ class NetTriggerDelegate
     
   def applicationDidFinishLaunching(notification)
     @monitor = WifiMonitor.alloc.initWithDelegate(self)
-    NSNotificationCenter.defaultCenter.addObserver(self,
-                                                  selector: "checkTaskStatus:".to_sym,
-                                                  name: NSTaskDidTerminateNotification,
-                                                  object: nil)
     activateStatusMenu
   end
   
@@ -16,14 +12,6 @@ class NetTriggerDelegate
   
   def showPreferencePanel(sender)
     preferenceController.showWindow(self)
-  end
-  
-  def checkTaskStatus(notification)
-     if (notification.object.terminationStatus == 0)
-         NSLog("Task succeeded.")
-     else
-         NSLog("Task failed.")
-    end
   end
   
   def wifiChangedTo(ssid)
